1be8d878208cc47e4eecde0f1d60b56975615ad9
Jeffrey Wilcke <geffobscura@gmail.com>
2016-02-20 23:15:16 +0100

    queue

--- balancer/balancer.go
+++ balancer/balancer.go
@@ -1,9 +1,6 @@
 package balancer
 
-import (
-	"container/heap"
-	"runtime"
-)
+import "runtime"
 
 var B = New(runtime.GOMAXPROCS(0))
 
@@ -32,39 +29,14 @@ type Worker struct {
 
 // work will take the oldest task and execute the function and
 // yield the result back in to the return error channel.
-func (w *Worker) work(done chan *Worker) {
-	for {
-		task := <-w.tasks   // get task...
-		task.c <- task.fn() // ...execute the task
-		done <- w           // we're done
+func (w *Worker) work(queue <-chan Task) {
+	for task := range queue {
+		task.c <- task.fn() // get task and execute
 	}
 }
 
-// Pool is a pool of workers and implements containers.Heap
+// Pool is a pool of workers
 type Pool []*Worker
-
-func (p Pool) Len() int           { return len(p) }
-func (p Pool) Less(i, j int) bool { return p[i].pending < p[j].pending }
-func (p Pool) Swap(i, j int) {
-	p[i].index = j // trade i<->j
-	p[j].index = i // trade j<->i
-
-	p[i], p[j] = p[j], p[i]
-}
-func (p *Pool) Push(x interface{}) {
-	w := x.(*Worker)  // cast the worker
-	w.index = len(*p) // assign the new index
-
-	*p = append(*p, x.(*Worker))
-}
-
-func (p *Pool) Pop() interface{} {
-	old := *p
-	n := len(old)
-	x := old[n-1]
-	*p = old[0 : n-1]
-	return x
-}
 
 // Balancer is responsible for balancing any given tasks
 // to the pool of workers. The workers are managed by the
@@ -72,19 +44,15 @@ func (p *Pool) Pop() interface{} {
 // equally balanced in "work to complete".
 type Balancer struct {
 	pool Pool
-	done chan *Worker
-
 	work chan Task
 }
 
 // New returns a new load balancer
 func New(poolSize int) *Balancer {
 	balancer := &Balancer{
-		done: make(chan *Worker),
-		work: make(chan Task),
-		pool: make(Pool, 0, poolSize),
+		work: make(chan Task, 1000),
+		pool: make(Pool, poolSize),
 	}
-	heap.Init(&balancer.pool)
 
 	// fill the pool with the given pool size
 	for i := 0; i < poolSize; i++ {
@@ -92,51 +60,15 @@ func New(poolSize int) *Balancer {
 		worker := &Worker{id: i, tasks: make(chan Task, 100)}
 		// spawn worker process
 		go func(i int) {
-			worker.work(balancer.done)
+			worker.work(balancer.work)
 		}(i)
-		heap.Push(&balancer.pool, worker)
+		balancer.pool[i] = worker
 	}
-	// spawn own balancer task
-	go balancer.balance(balancer.work)
 
 	return balancer
 }
 
 // Push pushes the given tasks in to the work channel.
 func (b *Balancer) Push(work Task) {
-	go func() { b.work <- work }()
-}
-
-func (b *Balancer) balance(work chan Task) {
-	for {
-		select {
-		case w := <-b.done: // worker is done
-			b.completed(w) // handle worker
-		case task := <-work: // get task
-			b.dispatch(task) // dispatch the tasks
-		}
-	}
-}
-
-// dispatch dispatches the tasks to the least loaded worker.
-func (b *Balancer) dispatch(task Task) {
-	// Take least loaded worker
-	w := heap.Pop(&b.pool).(*Worker)
-	// send it a task
-	w.tasks <- task
-	// add to its queue
-	w.pending++
-	// put it back in the heap
-	heap.Push(&b.pool, w)
-}
-
-// completed handles the worker and puts it back in the pool
-// based on it's load.
-func (b *Balancer) completed(w *Worker) {
-	// reduce one task
-	w.pending--
-	// remove it from the heap
-	heap.Remove(&b.pool, w.index)
-	// put it back in place
-	heap.Push(&b.pool, w)
+	b.work <- work
 }
